9bf5bb28fc15d75a9780ce0a4faa99db9055f572
Davis2200 <dnavaa2200@alumno.ipn.mx>
2026-08-08 14:21:19 -0600

    permiso para frontend par ingresar a backend

--- backend/api/main.py
+++ backend/api/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
+        "https://davidnava.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Permite POST, GET, OPTIONS, etc.
